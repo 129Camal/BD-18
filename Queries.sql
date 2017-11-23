3c40cb3ad045b8f561fd89a9849569a579a07415
Frederico Pinto <frederico21pinto@hotmail.com>
2017-11-23 00:39:03 +0000

    Tá fixe!

--- Queries.sql
+++ Queries.sql
@@ -4,3 +4,8 @@ select * from elemento;
 select * from equipa;
 select * from seccao;
 select * from elemento where Morada='Bairro 1º Dezembro, nº11';
+
+SELECT * FROM Elemento
+    INNER JOIN Seccao ON Seccao.Designacao = Elemento.Seccao_Designacao
+	WHERE 
+	Elemento.Cargo = "Chefe de Seccao" AND Elemento.Seccao_Designacao = "Lobitos";
