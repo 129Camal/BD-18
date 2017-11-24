362d7a620994a3f4aa9cbbcfcbd3458bbf1f67f9
Frederico Pinto <frederico21pinto@hotmail.com>
2017-11-24 20:41:08 +0000

    Remover lixo!

--- povoamento.sql
+++
@@ -1,8 +0,0 @@
-use agrup424;
-                
-insert into Elemento
-			(Nin, Nome, Idade, Sexo, DatadeNasc, Morada, Telemovel, Email, NoitesdeCampo, Funcao_Designacao, Seccao_Designacao, Equipa_idEquipa)
-			value
-				(1, 'Lucas Ribeiro Pereira', 23, 'M', 15-07-1994, 'Bairro 1º Dezembro, nº11 Nogueira Braga', 911117299,'lrpereiraa@gmail.com', 100, 'Chefe de Agrupamento', null , null)
-			;
-            
