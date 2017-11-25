fc65ee1b5a5a4b23833980a582d92a0587e5cf28
Ricardo Canela <ricardocanela.ns@gmail.com>
2017-11-25 20:16:20 +0000

    novo procedimento
    Idade secçao

--- FunçãoAtividade.sql
+++ FunçãoAtividade.sql
@@ -12,3 +12,15 @@ END
 DELIMITER // 
 
 CALL InsereAtividade('Escondidas','Integrar as pessoas no grupo')
+
+DELIMITER //
+CREATE procedure AtribuirSeccao (IN Idade VARCHAR(20),IN Nin int)
+
+BEGIN
+	SELECT Idade,Nome,Seccao_Designacao FROM Elemento;
+	IF Idade BETWEEN 6 AND 10 THEN
+	INSERT INTO Elemento(Seccao_Designacao)
+	VALUES ('Lobitos');
+    END IF;
+END 
+//
--- Queries.sql
+++ Queries.sql
@@ -19,8 +19,14 @@ SELECT * FROM Elemento
        INNER JOIN EQUIPA ON Equipa.idEquipa = Elemento.Equipa_idEquipa
        WHERE Elemento.Equipa_idEquipa = '2';
        
-SELECT Nin,Nome,funcao_designacao,seccao_designacao FROM Elemento
+SELECT Nin,Nome,funcao_designacao,seccao_designacao FROM Elemento;
 
-
-
+SELECT CONCAT(Elemento.Funcao_Designacao,Nome) AS LINDO FROM Elemento
+	WHERE Nome IN ('Joaquim Silva Antunes Pinto','Fernanda Carvalho');
+    
+SELECT Nin,Nome FROM Elemento
+	where Idade > (SELECT avg(Idade) FROM Elemento);
+    
+SELECT Designacao FROM Funcao
+	WHERE Designacao IN (LIKE'chefe'); 
 	
